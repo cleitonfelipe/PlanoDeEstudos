e963e5941be9500be647d3a248da64788c754924
Cleiton Felipe de Moraes <cleiton.felipe@yahoo.com.br>
2020-02-06 13:46:33 -0300

    Add project

---
+++ PlanoDeEstudos/PlanoDeEstudos/PlanoDeEstudos/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PlanoDeEstudos.Models;
+
+namespace PlanoDeEstudos.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
